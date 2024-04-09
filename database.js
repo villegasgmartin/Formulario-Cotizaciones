182e258418bc0d3b316cb8174843801c413535b2
MartinLuxury <mvillegas@luxurypresence.com>
2024-04-09 00:50:15 -0300

    credenciales

--- database.js
+++ database.js
@@ -4,14 +4,20 @@ const mysql = require('mysql2/promise');
 //equivalente a create connection, conecto a la base de datos
 
 
+// const config = {
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE, 
+//     port: process.env.PORTDB
+// }
 const config = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE, 
-    port: process.env.PORTDB
+    host: viaduct.proxy.rlwy.net,
+    user: root,
+    password: rEOQuIUHxHlTctuFKDdSYRbRyTDATTln,
+    database: railway, 
+    port:55697,
 }
-
 const pool = mysql.createPool(config);
 
 // Manejar eventos de error
